6f4093288c6b36bd3c47325be3fd1e2690a2dc5f
Sarah Carlson <sbcarls@gmail.com>
2021-01-02 21:19:31 -0500

    added db/schemas folder

--- server.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('.utils/helpers');
-
-const sequelize = require('./config/connection');
